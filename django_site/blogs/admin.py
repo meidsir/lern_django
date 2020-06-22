d9f0ce746178c5555d3ecd1488917373bd88dabe
meidsir <306055516@qq.com>
2020-06-22 16:02:52 +0800

    commit

--- django_site/blogs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Myblogs
-
-admin.site.register(Myblogs)
-from django.contrib import admin
-
-# Register your models here.
--- django_site/blogs/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("你好,这是你的博客页面.")
